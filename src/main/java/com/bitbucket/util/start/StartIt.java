4ca10a071c56a3a857b6fcb274d1d0415e4ca1e3
mishraRam <ram.m@emeasurematics.com>
2020-06-28 04:59:03 +0530

    added helper to fetch all the projects and also updated the UI to take pull request from user.

--- src/main/java/com/bitbucket/util/start/StartIt.java
+++ src/main/java/com/bitbucket/util/start/StartIt.java
@@ -1,5 +1,7 @@
 package com.bitbucket.util.start;
 
+import com.bitbucket.util.screen.UserInformation;
+
 /**
  *
  * @author Ram Mishra
@@ -8,5 +10,7 @@ public class StartIt {
 
     public static void main(String[] args) {
 
+        UserInformation bitBucketUserInfo = new UserInformation();
+        bitBucketUserInfo.build(new String[]{});
     }
 }
