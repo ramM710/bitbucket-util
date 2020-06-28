5ae50febc968eb5b58d8c2a406a9a97755abc9f6
Ritika Ghosh <Ritika.Ghosh@sterlingts.com>
2020-06-28 15:41:40 +0530

    missing changes

--- src/main/java/com/bitbucket/util/automate/webdriver/DesiredCapabilities.java
+++ src/main/java/com/bitbucket/util/automate/webdriver/DesiredCapabilities.java
@@ -5,18 +5,39 @@
  */
 package com.bitbucket.util.automate.webdriver;
 
+import java.util.Map;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.BrowserType;
 
 /**
  *
  * @author Ritika.Ghosh
  */
-public class DesiredCapabilities {
+public class DesiredCapabilities implements Capabilities{
    
 
   public DesiredCapabilities() {
     // no-arg constructor
   }
   
+   public DesiredCapabilities(String browser, String version, Platform platform) {
+    setCapability(BROWSER_NAME, browser);
+//    setCapability(VERSION, version);
+    setCapability(PLATFORM, platform);
+  }
   
+   public static DesiredCapabilities chrome() {
+    return new DesiredCapabilities(BrowserType.CHROME, "", Platform.ANY);
+  }
+
+    @Override
+    public Map<String, Object> asMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getCapability(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
